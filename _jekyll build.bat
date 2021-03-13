80fc034b04c0d9df62dae1f13c6614d8621578c3
OrionFive <wiltschek@gmx.net>
2021-03-13 20:57:22 +0100

    Created jekyll page

---
+++ _jekyll build.bat
@@ -0,0 +1,4 @@
+call E:\Ruby30-x64\bin\setrbvars.cmd
+call bundle exec jekyll build
+call bundle update github-pages
+pause
---
+++ _jekyll test.bat
@@ -0,0 +1,4 @@
+call E:\Ruby30-x64\bin\setrbvars.cmd
+start "" http://127.0.0.1:4000/
+call bundle exec jekyll serve --watch
+pause
