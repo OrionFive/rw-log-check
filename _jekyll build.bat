e3e3e71a3d6cba3585457097c92433d171d1bd0c
OrionFive <wiltschek@gmx.net>
2021-03-14 19:02:13 +0100

    Removed batch files

--- _jekyll build.bat
+++
@@ -1,4 +0,0 @@
-call E:\Ruby30-x64\bin\setrbvars.cmd
-call bundle exec jekyll build
-call bundle update github-pages
-pause
--- _jekyll test.bat
+++
@@ -1,4 +0,0 @@
-call E:\Ruby30-x64\bin\setrbvars.cmd
-start "" http://127.0.0.1:4000/
-call bundle exec jekyll serve --watch
-pause
