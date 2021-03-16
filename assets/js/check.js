476b177f561b8c2667673638c847251e826a9203
OrionFive <wiltschek@gmx.net>
2021-03-16 19:27:11 +0100

    Added output: runtime errors

--- assets/js/check.js
+++ assets/js/check.js
@@ -34,8 +34,92 @@ function checkResponse(response){
 }
 
 function parseUrl(data) {
-    console.log(data);
     successBox.style.display = "inherit";
-    // result = {loadPage};
-    // console.log(result);
+    const log = v.split(data, '\n');
+
+    checkLoadedGame(log);
+}
+
+function checkLoadedGame(log) {
+    const startLoadedIndex = log.findIndex(l => v.startsWith(l, "Loading game from file"));
+    const loadedLog = log.slice(startLoadedIndex);
+    
+    document.getElementById("output").style.visibility = "visible";
+
+    // RUNTIME
+    const mods = getLoadedMods(loadedLog);
+    outputList("active-mods-list", mods, newListItem);
+
+    const consoleLogs = getConsoleLogs(loadedLog);
+    console.log(consoleLogs);
+    const warnings = getWarnings(consoleLogs);
+    const errors = getErrors(consoleLogs);
+    const combined = warnings.concat(errors);
+    outputList("warnings-errors-runtime-list", combined, newLogListItem);
+}
+
+function getLoadedMods(loadedLog) {
+    loadedLog = loadedLog.slice(1); // skip first row
+    const lastModIndex = loadedLog.findIndex(l => !v.startsWith(l, "  ")); // find end of mod list
+    const loadedMods = loadedLog.slice(0, lastModIndex).map(mod => v.trimLeft(mod, ' -')); // remove end and trim away "  - "
+    return loadedMods;
+}
+
+function getConsoleLogs(loadedLog) {
+    loadedLog = loadedLog.slice(1); // skip first row
+    const lastModIndex = loadedLog.findIndex(l => !v.startsWith(l, "  ")); // find end of mod list
+    const sliced = loadedLog.slice(lastModIndex);
+    console.log(sliced);
+    return sliced.map((log, i) => parseOutput(sliced, i)).filter(log => log);
+}
+
+function getWarnings(consoleLogs) {
+    return consoleLogs.filter(log => log.type === "warning");
+}
+function getErrors(consoleLogs) {
+    return consoleLogs.filter(log => log.type === "error");
+}
+
+function parseOutput(consoleLogs, index) {
+    const current = consoleLogs[index];
+    const next = index < consoleLogs.length ? consoleLogs[index+1] : null;
+    
+    if(!current || current.startsWith("(") || current.startsWith(" ")) return null;
+    if(next != null && next.startsWith("(")) return {
+        type: "error",
+        content: current
+    }
+    else return {
+        type: "message",
+        content: current
+    }
+}
+
+function outputList(listId, items, constructor) {
+    const list = document.querySelector(`#${listId}`);
+    const header = list.querySelector("h3");
+
+    header.innerText += ` (${items.length})`;
+
+    const ul = document.createElement("ul");
+
+    items.forEach(item => {
+        ul.appendChild(constructor(item));
+    });
+    list.append(ul);
+}
+
+function newListItem(text) {
+    const li = document.createElement("li");
+    li.innerHTML = text;
+    return li;
+}
+
+function newLogListItem(logItem) {
+    const li = document.createElement("li");
+    li.innerHTML = logItem.content;
+    li.title = logItem.content;
+    li.classList.add(logItem.type);
+    console.log(li);
+    return li;
 }
