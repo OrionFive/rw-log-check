ec01a61c5150b0d8d64054063392ae186e1584c5
OrionFive <wiltschek@gmx.net>
2021-03-21 13:05:14 +0100

    Improved an error check

--- assets/js/logParser/errors/could-not-resolve-load-id.js
+++ assets/js/logParser/errors/could-not-resolve-load-id.js
@@ -1,10 +1,11 @@
 registerClassifierNormal((content, situation) => {
-    const check = "Could not resolve reference to object with loadID %s of type %s.";
+    const check = "Could not resolve reference to object with loadID %s of type %s. Was it compressed away, destroyed, had no ID number, or not saved/loaded right? curParent=%s.";
     const result = tryMatch(content, check);
     if(!result) return null;
     return {
         type: "error",
         content: result,
-        explanation: "An object can't be found. Could come from a removed, updated, or broken mod. Can be an indicator of a broken save file."
+        explanation: "An object can't be found. Could come from a removed, updated, or broken mod. Can be an indicator of a broken save file.",
+        offendingModIds: [result[3]]
     }
 });
